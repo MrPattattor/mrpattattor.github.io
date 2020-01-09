d1dd89c963a518e947d338dfc08369c9d45716a4
MrPattattor <57596685+MrPattattor@users.noreply.github.com>
2020-01-09 16:06:36 +0100

    Changement nom application plus géolocalisation

--- js/main.js
+++ js/main.js
@@ -5,4 +5,20 @@ window.onload = () => {
     navigator.serviceWorker
              .register('./sw.js');
   }
-}
+  //Fonction qui récupère la géolocalisation et la retourne
+  function getLocation() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+      } else {
+        console.log("Geo Location not supported by browser");
+      }
+  }//function that retrieves the position
+  function showPosition(position) {
+    var location = {
+      longitude: position.coords.longitude,
+      latitude: position.coords.latitude
+    }
+  console.log(location)
+  }//request for location
+  getLocation();
+  }
